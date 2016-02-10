06edc956c5abcbd9cd3fb82e4ce2e850781e1682
alecor <alecor.dev@gmail.com>
2016-02-10 21:55:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Navigator
+
+Simple example of a data-driven macro to navigate the web.
+
+## Dependencies
+
+- selenium
+- pyyaml
---
+++ navigator.py
@@ -0,0 +1,47 @@
+import time
+import logging
+import yaml
+
+from selenium import webdriver
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+class SetupDriver():
+
+    def __init__(self, browser):
+        if browser.lower() == 'firefox':
+            self.driver = webdriver.Firefox()
+
+    def __enter__(self):
+        return self.driver
+
+    def __exit__(self, *args):
+        self.driver.quit()
+
+with open('config.yaml') as f:
+    config = yaml.load(f)
+
+with open('macros.yaml') as f:
+    macros = yaml.load(f)
+
+def process(steps):
+    with SetupDriver('firefox') as driver:
+        for i, action in enumerate(steps):
+            print(i, action)
+            try:
+                if action.get('open'):
+                    driver.get(action['open'])
+                elif action.get('wait'):
+                    time.sleep(action['wait'])
+                # elif action.get('click'):
+                #     driver.click('element')
+            except Exception as e:
+                logger.error(e)
+
+defined_actions = set(['open', 'click', 'wait'])
+
+for macro_counter, macro in enumerate(macros['macros']):
+    print('Macro #{c}'.format(c=macro_counter))
+    for properties in macro:
+        process(macro[properties]['steps'])
